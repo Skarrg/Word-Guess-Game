d07036f77f3ce1c553e0154973111bb2454ba9dd
Skarrggit config --global user.name Skarrggit config --global user.name Skarrg <orestes.moiran@gmail.com>
2018-10-02 21:14:35 -0700

    Starting off

---
+++ assets/javascript/game.js
@@ -0,0 +1,18 @@
+window.onload = function () {
+
+    var alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
+
+    var word;               //Current word
+    var guess;              //Most recent guess
+    var guesses = [];       //previous guesses
+    var wins;
+    var lives;              //#of guesses left
+    var correctGuesses;
+    var space;              //spaces in the word
+
+    //onkey up for guess
+    object.onkeyup = function makeGuess() {
+        var guess = document.getElementById("#").value;
+        document
+    }
+}
