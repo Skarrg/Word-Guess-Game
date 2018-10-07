8249fec310d57b1d316c576657638137f2b26692
Skarrggit config --global user.name Skarrggit config --global user.name Skarrg <orestes.moiran@gmail.com>
2018-10-06 18:03:47 -0700

    IT LIVES MUAHAHAHA

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,36 +1,31 @@
 window.onload = function () {
 
     var alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
-    var wordlist = ['razputin', 'reynold', 'wren', 'everett', 'charlie', 'lucy',
-        'lili', 'boyd', 'oleander', 'milla', 'sasha', 'ford', 'dogen', 'napoleon',
-        'bobby', 'crispin', 'loboto', 'sheegor', 'kochamara', 'linda', 'pokeylope',
-        'gloria', 'edgar', 'benny', 'elka', 'kitty', 'chloe', 'jt', 'quentin',
-        'vernon', 'milka', 'crystal', 'clem', 'nils', 'maloof', 'mikhail', 'pheobe',
-        'elton', 'chops', 'franke', 'eddie', 'ophelia', 'killmaster', 'lionwhyte',
-        'doviculus', 'kage', 'mangus', 'rima', 'fletus', 'lars', 'lita', 'baron',
-        'ormagoden', 'mittens', 'guardian', 'succoria', 'dorsilla', 'shay', 'vella',
-        'marek'];
+    var wordlist = ['duckling', 'chipmunk', 'flamingo', 'blowfish', 'numbat', 'nightjar', 'ovenbird', 'lovebird', 'hamster', 'leopard'];
 
     var word;               //Current word
     var guess;              //Most recent guess
     var guesses = [];       //previous guesses
-    var wins;
-    var lives = 10;         //#of guesses total
+    var wins = 0;
+    var lives = 15;         //#of guesses total
     var livesLeft;          //guesses remaining
-    var correctGuesses;
-    var space;              //spaces in the word
-    //Word selected at start
-    var word = wordlist[Math.floor(Math.random() * wordlist.length)];
+    var correctGuesses = 0;
+    var chosenWordA = [];
+    //spaces in the word
 
-    //change word to an array to turn into _
-    var chosenWordA = [...word];
-    var lettersRemaining = chosenWordA.length;
+    //Word selected at start
+    function WordSelect() {
+        word = wordlist[Math.floor(Math.random() * wordlist.length)];
+        //change word to an array to turn into _
+        chosenWordA = [...word];
+    }
+
     //display chosen word array as _s
     function displayUnderscores() {
         for (var i = 0; i < chosenWordA.length; i++) {
             var newDiv = document.createElement('div');
             size = wordbox.offsetWidth / chosenWordA.length;
-           
+
             //format it it up
             newDiv.innerHTML = "_";
             newDiv.style.width = 0.8 * size + 'px';
@@ -47,53 +42,79 @@ window.onload = function () {
         }
     }
 
-    //identify unique characters in the array
-    function unique_char(str1) {
-        var str = str1;
-        var uniqueL = [];
-        for (var i = 0; i < str.length; i++) {
-            if (uniqueL.indexOf(str.charAt(i)) == -1) {
-                uniqueL += str[i];
-            }
+    //function to clear and reset everything
+    function NewRound() {
+        var guessesDiv = document.getElementById("guessesbox");
+        var guessDiv = document.getElementById("guessbox");
+        var wordboxDiv = document.getElementById("wordbox");
+        var livesboxDiv = document.getElementById("livesbox");
+        var winsboxDiv = document.getElementById("winsbox");
+
+        correctGuesses = 0;
+        livesLeft = 15;
+
+        guessDiv.innerHTML = "Your Most Recent Guess:";
+        guessesDiv.innerHTML = "Previous Guesses:";
+        while (wordboxDiv.firstChild) {
+            wordboxDiv.removeChild(wordboxDiv.firstChild);
         }
-        return uniqueL;
+        livesboxDiv.innerHTML = "Guesses left:";
+        winsboxDiv.innerHTML = "Wins: " + ++wins;
+        WordSelect();
+        displayUnderscores(chosenWordA);
     }
 
     //check to see if pressed key is in the word
     function CheckGuess(guess, chosenWordA) {
+        var letterDiv = document.getElementsByClassName(guess);
+        var guessesDiv = document.getElementById("guessesbox");
+        var guessDiv = document.getElementById("guessbox");
+
         //=== -1 means if it's not in the array
-        if (chosenWordA.indexof(input) === -1){
+        if (chosenWordA.indexOf(guess) === -1) {
 
         } else {
-            var letterDiv = document.getElementById(chosenWordA).querySelectorAll('.' + guess);
-            for (var i = 0; i<letterDiv.length; i++) {
-                letterDiv[i].innerHTML = guess;
-            }
+            letterDiv[0].innerHTML = guess;
+            ++correctGuesses;
         }
+
+        guessesDiv.innerText = 'Previous Guesses: ' + guesses;
+        guessDiv.innerText = 'Your Most Recent Guess: ' + guess;
+
+        //for (var i = 0; i < letterDiv.length; i++) {
+        //    if
+        //}
     }
 
     //onkey up for guess and put previous guess into Guesses
     document.onkeyup = function (event) {
-        var guess = event.key.toLowerCase();
-        if (alphabet.indexOf(guess) === -1){
+        guess = event.key.toLowerCase();
+        var guessesLeftDiv = document.getElementById("livesbox");
+        if (alphabet.indexOf(guess) === -1) {
             alert("Whoops! That button wasn't a letter! please try again.")
-        } 
-
-        if (guesses.indexOf(guess) === -1 && alphabet.indexOf !== -1){
-            guesses.push(guess);
-            livesLeft = lives - guesses.length;
         }
 
-        var win = guesses.filter(function(e) {
-            return unique_char(chosenWordA).indexOf(e) < -1;
-        });
+        if (guesses.indexOf(guess) === -1 && alphabet.indexOf !== -1) {
+            guesses.push(guess);
+            livesLeft = lives - guesses.length;
+            guessesLeftDiv.innerText = 'Guesses left: ' + livesLeft;
+            CheckGuess(guess, chosenWordA);
+        }
 
-        CheckGuess(guess, chosenWordA);
-        
+        if ((correctGuesses) >= chosenWordA.length) {
+            alert("YOU WIN! The word was " + word);
+            NewRound();
+        }
+
+        if (livesLeft == 0) {
+            alert('You ran out of guesses! Try again.')
+            NewRound();
+        }
     }
 
-
-
     //game start
+    WordSelect();
     displayUnderscores(chosenWordA);
+
+    
 }
